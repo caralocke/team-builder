4769430152cac5f3b5f2498bb1d239288a0f95c2
caralocke <cara.m.locke@gmail.com>
2021-05-18 16:21:16 -0400

    Was able to get inputs to populate as they should and it appears the state is changing as it should in the components tab of React Dev Tools

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
-import React from 'react'
+import React, {useState} from 'react'
 import TeamMember from './components/TeamMember'
-import TeamMemberForm from './components/TeamMemberForm'
+import Form from './components/Form'
+import axios from 'axios'
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  role: '',
+}
 
 function App() {
+  const [teamMembers, setTeamMembers] = useState([])
+
+  //Creating a state to hold all the values of the form
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({...formValues, [inputName]: inputValue})
+  }
+  
+  const submitForm = () => {
+    const newTeamMember = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+    }
+    if (!newTeamMember.name || !newTeamMember.email || ~newTeamMember.role)
+    return
+    axios.post('fakeapi.com', newTeamMember)
+    .then(res => {
+      console.log('res.data: \n', res.data)
+      const teamMemberFromBackEnd = res.data
+      setTeamMembers([teamMemberFromBackEnd, ...teamMembers])
+      setFormValues(initialFormValues)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form update={updateForm} submit={submitForm} values={formValues}/>
+     <TeamMember />
     </div>
   );
 }
